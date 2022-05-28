436ff78280116627b6b1244d4b6a0c653a65ab3d
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-05-28 14:48:31 +0500

    new created

---
+++ list_comprehenssion.py
@@ -0,0 +1,19 @@
+# l=[]
+# for i in range(1,101):
+#    l.append(i)
+#    print(l)
+
+# 2nd and fast method to do this is :
+
+n=[m for m in range(1,101)]
+print(n)
+# only even no.
+i=[n for n in range(1,101) if n%2==0]
+print(i)
+
+
+ 
+# string
+s="usman javaid"
+n=[m for m in s]
+print(n)
---
+++ list_iteration.py
@@ -0,0 +1,15 @@
+w=[1,9,10,3,1,12,3,2,4]    # reverse   order   =>    [::-1]
+l=len(w)
+print(l)
+for i in range(l):
+    print(w[i])
+    # for reverse order => 
+for i in range(l-1,-1,-1):
+    print(w[i])
+
+print("\n")
+w=[1,5,'usman',4,1,3,1,]
+
+for i in w:
+    print(i)
+
