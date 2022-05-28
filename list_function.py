544447e8dd013f3320d3200e31c2aa0c9e79bdfd
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-05-28 16:49:57 +0500

    new create

---
+++ list_function.py
@@ -0,0 +1,23 @@
+l=[10,20,30,40,50]
+# del l[1]
+print(l)
+
+# print("pop value: ",l.pop(1))
+# print(l)
+
+# l.remove(10)
+# print(l)
+
+# l.clear()
+# print(l)
+
+# l.insert(0,70)
+# print(l)
+
+a=[30,40,50,70]
+# l.append(a)
+# print(l)
+
+
+l.extend(a)
+print(l)
