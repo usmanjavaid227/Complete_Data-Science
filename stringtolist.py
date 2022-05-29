62897abea88a465825f081a7e0325919359a5804
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-05-29 09:06:32 +0500

    new programs

---
+++ stringtolist.py
@@ -0,0 +1,11 @@
+####for single line of strings
+# a=input("Enter the string")
+# l=a.split()
+# print(l)
+
+l=[]
+for i in range(1,4):
+    n=input("Enter the string "+str(i)+" :- ")
+    l.append(n)
+print(l)
+    
