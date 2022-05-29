ffef66e36c9e9257519f0e066303c0c0a5a344b1
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-05-29 11:26:18 +0500

    file

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+d={ 
+      'name':'usman',
+       'age':20
+}
+# print(type(d))
+# print(d)
+# print(d['name'])
+
+for a in d:
+    print(d[a])
---
+++ dictionary_fn.py
@@ -0,0 +1,32 @@
+a={
+    'name':'usman',
+    'age':20
+}
+# b=a.get('name')
+# print(b)
+
+# for z in a.keys():
+#     print(z)
+    
+# for z in a.values():
+#     print(z)
+
+for z in a.items():
+    print(z)
+
+# del a['age']
+# print(a)
+
+# x=a.pop('name')
+# print(x)
+# print(a)
+
+# a=dict(name='usman', age=20)
+# print(a)
+
+# a.update({"name":'javaid'})
+# a['name']='usman'
+# print(a)
+
+# a.clear()
+# print(a)
