feaed587359cbb45a84ba32afd60c7f1ace726c0
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-05-29 16:05:34 +0500

    functionFile

---
+++ functions.py
@@ -0,0 +1,18 @@
+# def hello():
+#     print("Hello world!")
+# hello()
+
+# def sum(a, b):
+#     print("Sum is :",a+b)
+# sum(5,7)
+
+# def sum(a=0, b=0):
+#     print("Sum is :",a+b)
+# sum(7,8)
+
+def rtype(a):
+    return a*a
+    
+n=int(input("enter no. "))
+ans=rtype(n)
+print(ans)
