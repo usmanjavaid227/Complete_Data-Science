dfa1a5588c2163fc5b769efc529ddb1734dd4497
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-05-29 14:46:25 +0500

    new

---
+++ sets.py
@@ -0,0 +1,23 @@
+p={1,2,3,4}
+
+l=[11,12,13,14]
+s=set(l)
+# print(s)
+
+# s.add(20)
+# print(s)
+
+# s.pop()
+# print(s)
+
+# s.remove(12)
+# print(s)
+
+# s.discard(12)
+# print(s)
+
+# s.clear()
+# print(s)
+
+s.update(p)
+print(s)
---
+++ tuple.py
@@ -0,0 +1,28 @@
+t=(10,20,30,40,10,50)
+# print(t[0])
+# print(t)
+l=len(t)
+# print(l)
+
+# for i in range(l):
+#     print(t[i])
+
+# for i in t:
+#     print(i)
+ 
+# m=min(t)
+# print(m)
+
+# m=max(t)
+# print(m)
+
+# c=t.count(10)
+# print(c)
+
+# print(t.index(10))
+
+s=sum(t)
+print(s)
+
+s=sum(t,10)
+print(s)
