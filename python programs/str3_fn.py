eb59ac75e6232d99cb10fa12b80498cadf22e6b0
usmanjavaid227 <71181962+usmanjavaid227@users.noreply.github.com>
2022-07-07 14:21:15 +0500

    hg

--- python programs/str3_fn.py
+++ python programs/str3_fn.py
@@ -1,10 +1,11 @@
 a="I am {:^10} {:>10}".format('usman','Javaid')
-a="I am {} {}".format('usman','Javaid')
-a="I am {0} {1}".format('usman','Javaid')
-a="I am {fname} {lname}".format(fname='usman',lname='Javaid')
+# a="I am {} {}".format('usman','Javaid')
+# a="I am {0} {1}".format('usman','Javaid')
+# a="I am {fname} {lname}".format(fname='usman',lname='Javaid')
 print(a)
 
-### use
+### use for space
 # ^ in between
 # > for right
 # < for left
+# it takes 10 places to print usman
